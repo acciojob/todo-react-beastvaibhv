c06c7545df0e2dbee3a194cfa0335c691b3ae290
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-12-19 15:02:13 +0000

    todo-list-first-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [task, setTask] = useState([]);
+  let [text, setText] = useState("");
+  const handleTask=(e)=>{
+    const t = e.target.value;
+    setText(t);
+  }
+  const handleAddingTask =()=>{
+    if(text.trim()!== ""){
+    setTask(prevItems=>[...prevItems, text]);
+    }
+    setText("");
+   
+  }
+  const handleDelete=(index)=>{
+       let newArr = [...task];
+       newArr.splice(index, 1);
+       setTask(newArr);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+        <input type="text" onChange={handleTask} value={text}  /> <button onClick={handleAddingTask}>Add Todo</button>
+        <ul style={{listStyleType:"numeric"}}>
+          {task.map((item, index)=>(<li key={index}>  <p className="task">{item}</p>  <button onClick={handleDelete}>Delete</button></li>))}
+        </ul>
     </div>
   )
 }
